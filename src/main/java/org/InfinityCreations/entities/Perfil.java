9c574e95f7ecd5038817eb3f52475090335fb283
cristian <chiat.contact@gmail.com>
2024-06-13 12:22:54 -0400

    new entities

---
+++ src/main/java/org/InfinityCreations/entities/Perfil.java
@@ -0,0 +1,42 @@
+package org.InfinityCreations.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Perfil {
+    @Id
+    private long id;
+    private String nombre;
+    private String descripcion;
+
+    public Perfil(long id, String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
---
+++ src/main/java/org/InfinityCreations/entities/Raza.java
@@ -0,0 +1,52 @@
+package org.InfinityCreations.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Raza {
+    @Id
+    private long id;
+    private String nombre;
+    private String descripcion;
+    private String imagen;
+
+    public Raza(long id, String nombre, String descripcion, String imagen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+}
